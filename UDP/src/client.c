fdb4c59212b0b9c82f0375d0c2a281d3fa248f22
Kalle Møller <github.com@k-moeller.dk>
2015-10-26 10:35:01 +0100

    client

--- UDP/src/client.c
+++ UDP/src/client.c
@@ -11,6 +11,9 @@
 #define BUFFER_SIZE 1024
 #define PORT 9999
 
+#define IP_ARG 1
+#define CMD_ARG 2
+
 int main(int argc, char *argv[]) {
     struct sockaddr_in udpserv;
     int len = sizeof(struct sockaddr_in);
@@ -18,19 +21,21 @@ int main(int argc, char *argv[]) {
     struct hostent *host;
     int n, s;
 
-    s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-	
+    host = gethostbyname(argv[IP_ARG]);
+
+    s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
     memset((char *) &udpserv, 0, len);
     udpserv.sin_family = AF_INET;
     udpserv.sin_port = htons(PORT);
     udpserv.sin_addr = *((struct in_addr*) host->h_addr);
-    sendto(s, argv[3], strlen(argv[3]), 0, (struct sockaddr *) &udpserv, len)
-	
+    sendto(s, argv[CMD_ARG], strlen(argv[CMD_ARG]), 0, (struct sockaddr *) &udpserv, len);
+
     while ((n = recvfrom(s, buf, BUFFER_SIZE, 0, (struct sockaddr *) &udpserv, &len)) != -1) {
-		printf("Modtaget %s:%d: ", inet_ntoa(udpserv.sin_addr), ntohs(udpserv.sin_port)); 
-		fflush(stdout);
-		write(1, buf, n);
-		write(1, "\n", 1);
+                printf("Modtaget %s:%d: ", inet_ntoa(udpserv.sin_addr), ntohs(udpserv.sin_port));
+                fflush(stdout);
+                write(1, buf, n);
+                write(1, "\n", 1);
     }
 
     close(s);
