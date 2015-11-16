fa6266314174264307b1da57a011669a3395da3b
KasperSK <raizzat@gmail.com>
2015-11-16 14:04:31 +0100

    Hat

--- ProtocolStack/ProtocolStack.Program/Program.cs
+++ ProtocolStack/ProtocolStack.Program/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LinkLayer;
+using System.Threading;
 
 namespace ProtocolStack.Program
 {
@@ -14,6 +15,12 @@ namespace ProtocolStack.Program
             var Link = new Link(1000, new DecryptStm(), new EncryptStm(), new Serial("/dev/ttyS1", 115200, 8));
             var msg = new byte[10] {75, 65, 76, 76, 69, 66, 97, 108, 108, 101};
             Link.SendMessage(msg, 10);
+            var read = new byte[20];
+            while (Link.GetMessage(read) == 0)
+            {
+                Console.WriteLine("Trying to read!");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
