5b6f9bb666fc5027c4f217f38ab74baec7c941fe
KasperSK <raizzat@gmail.com>
2015-11-16 13:44:19 +0100

    Test und seriel

---
+++ ProtocolStack/Link/Serial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkLayer
+{
+    public class Serial : IPhysical
+    {
+        private SerialPort _port;
+
+        public Serial(string portName, int baud, int databits)
+        {
+            _port = new SerialPort(portName, baud, Parity.None, databits, StopBits.One);
+        }
+        public void Write(byte[] buffer, int buffersize)
+        {
+            _port.Write(buffer, 0, buffersize);
+        }
+
+        public int Read(byte[] buffer, int buffersize)
+        {
+            return _port.Read(buffer, 0, buffersize);
+        }
+    }
+}
---
+++ ProtocolStack/ProtocolStack.Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkLayer;
+
+namespace ProtocolStack.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Link = new Link(1000, new DecryptStm(), new EncryptStm(), new Serial("/dev/ttyS1", 115200, 8));
+            var msg = new byte[10] {75, 65, 76, 76, 69, 66, 97, 108, 108, 101};
+            Link.SendMessage(msg, 10);
+        }
+    }
+}
